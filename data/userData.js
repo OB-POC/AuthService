2f5b7ce013baaff3dce0b8c88c0c4bfdcdb9204d
Prithvi Raj Vuppalapati <prithvirajvuppalapati@Prithvis-MacBook-Pro.local>
2018-10-04 22:23:57 +0530

    userData Stub edited

--- data/userData.js
+++ data/userData.js
@@ -6,42 +6,55 @@ module.exports = {
             UIN: 'UIN-xxxx',
             creditScore: 700,
             debitBankAccountDetails: [
-                {
-                    bankName: 'Halifax',
-                    bankId: 'HHaalliiffaaxx',
-                    accounts: [
-                        {
-                            accountType: 'PCA',
-                            balance: 15000,
-                            minBalance: 3000,
-                            interestRate: 0                        
-                        }
-                    ]
-                },
-                {
-                    bankName: 'HDFC',
-                    bankId: 'HHDDFFCC',
-                    accounts: [
-                        {
-                            accountType: 'PCA',
-                            balance: 5000,
-                            minBalance: 3000,
-                            interestRate: 0                       
-                        }
-                    ]
-                },
-                {
-                    bankName: 'HSBC',
-                    bankId: 'HHSSBBCC',
-                    accounts: [
-                        {
-                            accountType: 'Savings',
-                            balance: 5000,
-                            minBalance: 4000,
-                            interestRate: 1                        
-                        }
-                    ]
-                }
+                    {
+                        "bankName": "Halifax",
+                        "bankId": "HHaalliiffaaxx",
+                        "accounts": [
+                            {
+                                "accountType": "PCA",
+                                "accountNumber": "123456 78901234",
+                                "accountTitle": "Classic Account",
+                                "standingInst": 300,
+                                "balance": 1500,
+                                "minBalance": 300,
+                                "interestRate": 0 ,
+                                "availableBalance":900
+                            }
+                        ]
+                    },
+                    {
+                        "bankName": "Barclays",
+                        "bankId": "Barclays",
+                        "accounts": [
+                            {
+                                "aaccountType": "PCA",
+                                "accountNumber": "234567 89012345",
+                                "accountTitle": "Platinum Account",
+                                "standingInst": 1000,
+                                "balance": 5000,
+                                "minBalance": 300,
+                                "interestRate": 0,
+                                "availableBalance":3700
+                            }
+                        ]
+                    },
+                    {
+                        "bankName": "Monzo",
+                        "bankId": "Monzo",
+                        "accounts": [
+                            {
+                                "accountType": "Savings",
+                                "accountNumber": "345678 90123456",
+                                "accountTitle": "Easy Saver",
+                                "standingInst": 700,
+                                "balance": 5000,
+                                "minBalance": 400,
+                                "interestRate": 1,
+                                "availableBalance":3900
+                            }
+                        ]
+                    }
+            
             ],
             creditBankAccountDetails: [
                 {
@@ -50,6 +63,10 @@ module.exports = {
                     accounts: [
                         {
                             accountType: 'CC',
+                            accountNumber: '456789 0123456789',
+                            accountTitle: 'Low Rate',
+                            creditLimit: 20000,
+                            availableCredit: 15000,
                             totalBalanceDue: 5000,
                             minBalanceDue: 500,
                             apr: 30,
@@ -58,11 +75,15 @@ module.exports = {
                     ]
                 },
                 {
-                    bankName: 'Capital One',
-                    bankId: 'Ccaappiittaall  OOnnee',
+                    bankName: 'Citi Bank',
+                    bankId: 'CCiittii  BBaannkk',
                     accounts: [
                         {
                             accountType: 'CC',
+                            accountNumber: '567890 1234567890',
+                            accountTitle: 'No Fee 0% Balance Transfer',
+                            creditLimit: 15000,
+                            availableCredit: 7000,
                             totalBalanceDue: 8000,
                             minBalanceDue: 3000,
                             apr: 26,
@@ -76,6 +97,10 @@ module.exports = {
                     accounts: [
                         {
                             accountType: 'M',
+                            accountNumber: '567890 1234567890',
+                            accountTitle: 'Home Loan',
+                            creditLimit: 15000,
+                            availableCredit: 8000,
                             totalBalanceDue: 7000,
                             minBalanceDue: 2000,
                             apr: 24,
