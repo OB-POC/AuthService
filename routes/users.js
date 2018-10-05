9bd5bf04cd6b34b3ac22f3c6c634e792cb9598f6
vinitkumargoel <vinit928@gmail.com>
2018-10-05 15:28:38 +0530

    commit for test

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* home end point */
 router.get('/', function(req, res, next) {
-    res.render('index', {title: 'Express'})
+    res.render('index', {title: 'Test Passed'})
 });
 
 module.exports = router;
