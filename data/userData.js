040ebf8f0794d1e9099bafe65db651b01265a1db
Prithvi Raj Vuppalapati <prithvirajvuppalapati@l-156083504.wipro.com>
2018-10-06 17:27:30 +0530

    real data added

--- data/userData.js
+++ data/userData.js
@@ -11,46 +11,46 @@ module.exports = {
                         "bankId": "HHaalliiffaaxx",
                         "accounts": [
                             {
-                                "accountType": "PCA",
+                                "accountType": "SB",
                                 "accountNumber": "123456 78901234",
-                                "accountTitle": "Classic Account",
-                                "standingInst": 500,
-                                "balance": 2500,
-                                "minBalance": 100,
-                                "interestRate": 0 ,
-                                "availableBalance":1900
+                                "accountTitle": "Every Day Saver",
+                                "standingInst": 2000,
+                                "balance": 4000,
+                                "minBalance": 1000,
+                                "interestRate": 0.2 ,
+                                "availableBalance":1000
+                            }
+                        ]
+                    },
+                    {
+                        "bankName": "HSBC",
+                        "bankId": "HHSSBBCC",
+                        "accounts": [
+                            {
+                                "accountType": "PCA",
+                                "accountNumber": "234567 89012345",
+                                "accountTitle": "HSBC Advance Bank Account",
+                                "standingInst": 1200,
+                                "balance": 4000,
+                                "minBalance": 200,
+                                "interestRate": 0.5,
+                                "availableBalance":2600
                             }
                         ]
                     },
                     {
                         "bankName": "Barclays",
-                        "bankId": "Barclays",
+                        "bankId": "BBaarrccllaayyss",
                         "accounts": [
                             {
-                                "accountType": "PCA",
-                                "accountNumber": "234567 89012345",
-                                "accountTitle": "Platinum Account",
-                                "standingInst": 500,
-                                "balance": 2000,
-                                "minBalance": 100,
-                                "interestRate": 0,
-                                "availableBalance":1400
-                            }
-                        ]
-                    },
-                    {
-                        "bankName": "Monzo",
-                        "bankId": "Monzo",
-                        "accounts": [
-                            {
-                                "accountType": "Savings",
+                                "accountType": "SB",
                                 "accountNumber": "345678 90123456",
                                 "accountTitle": "Easy Saver",
-                                "standingInst": 500,
-                                "balance": 5000,
-                                "minBalance": 100,
-                                "interestRate": 1,
-                                "availableBalance":4400
+                                "standingInst": 800,
+                                "balance": 6000,
+                                "minBalance": 200,
+                                "interestRate": 0.25,
+                                "availableBalance":5000
                             }
                         ]
                     }
@@ -58,53 +58,51 @@ module.exports = {
             ],
             creditBankAccountDetails: [
                 {
-                    bankName: 'Barclays',
-                    bankId: 'BBaarrccllaayyss',
+                    bankName: 'JP Morgan Chase',
+                    bankId: 'JJPP  MMoorrggaann  CChhaassee',
                     accounts: [
                         {
                             accountType: 'CC',
                             accountNumber: '456789 0123456789',
-                            accountTitle: 'Low Rate',
+                            accountTitle: 'Chase Freedom Unlimited Credit Card',
                             creditLimit: 3000,
-                            availableCredit: 1000,
-                            totalBalanceDue: 2000,
-                            minBalanceDue: 500,
-                            apr: 35,
+                            availableCredit: 1200,
+                            totalBalanceDue: 1800,
+                            minBalanceDue: 30,
+                            apr: 25.49,
                             dueDate: new Date().setDate(new Date().getDate()+5)                         
                         }
                     ]
                 },
                 {
-                    bankName: 'Citi Bank',
-                    bankId: 'CCiittii  BBaannkk',
+                    bankName: 'RBS',
+                    bankId: 'RRBBSS',
                     accounts: [
                         {
                             accountType: 'CC',
                             accountNumber: '567890 1234567890',
-                            accountTitle: 'No Fee 0% Balance Transfer',
+                            accountTitle: 'RBS Unlimited Credit Card',
                             creditLimit: 4000,
-                            availableCredit: 1500,
-                            totalBalanceDue: 2500,
-                            minBalanceDue: 400,
-                            apr: 25,
+                            availableCredit: 3800,
+                            totalBalanceDue: 200,
+                            minBalanceDue: 100,
+                            apr: 19.94,
                             dueDate: new Date().setDate(new Date().getDate()+12)                          
                         }
                     ]
                 },
                 {
-                    bankName: 'HSBC',
-                    bankId: 'HHSSBBCC',
+                    bankName: 'Monzo',
+                    bankId: 'MMoonnzzoo',
                     accounts: [
                         {
                             accountType: 'M',
                             accountNumber: '567890 1234567890',
                             accountTitle: 'Home Loan',
-                            minMonthlyPayment: 150,
-                            remainingFullTerm: '2Y 3M',
-                            totalBalanceDue: 2000,
-                            minBalanceDue: 2000,
-                            interestRate: 2.5,
-                            apr: 30,
+                            minMonthlyPayment: 900,
+                            remainingFullTerm: '0Y 5M',
+                            totalBalanceDue: 6000,
+                            interestRate: 2.90,
                             dueDate: new Date().setDate(new Date().getDate()+20)                         
                         }
                     ]
@@ -118,18 +116,18 @@ module.exports = {
             creditScore: 650,
             debitBankAccountDetails: [
                     {
-                        "bankName": "BOS",
-                        "bankId": "BBOOSS",
+                        "bankName": "Halifax",
+                        "bankId": "HHaalliiffaaxx",
                         "accounts": [
                             {
-                                "accountType": "PCA",
+                                "accountType": "SB",
                                 "accountNumber": "678901 23456789",
-                                "accountTitle": "Classic Account",
-                                "standingInst": 300,
-                                "balance": 2300,
-                                "minBalance": 100,
-                                "interestRate": 0 ,
-                                "availableBalance":1900
+                                "accountTitle": "Every Day Saver",
+                                "standingInst": 1000,
+                                "balance": 4000,
+                                "minBalance": 500,
+                                "interestRate": 0.20 ,
+                                "availableBalance":2500
                             }
                         ]
                     },
@@ -140,28 +138,28 @@ module.exports = {
                             {
                                 "aaccountType": "PCA",
                                 "accountNumber": "234567 89012345",
-                                "accountTitle": "Platinum Account",
-                                "standingInst": 500,
-                                "balance": 2000,
-                                "minBalance": 100,
-                                "interestRate": 0,
-                                "availableBalance":1400
+                                "accountTitle": "HSBC Advance Bank Account",
+                                "standingInst": 600,
+                                "balance": 3000,
+                                "minBalance": 300,
+                                "interestRate": 0.5,
+                                "availableBalance":2100
                             }
                         ]
                     },
                     {
-                        "bankName": "Monzo",
-                        "bankId": "Monzo",
+                        "bankName": "Barclays",
+                        "bankId": "BBaarrccllaayyss",
                         "accounts": [
                             {
-                                "accountType": "Savings",
+                                "accountType": "SB",
                                 "accountNumber": "345678 90123456",
                                 "accountTitle": "Easy Saver",
-                                "standingInst": 1700,
-                                "balance": 5000,
-                                "minBalance": 300,
-                                "interestRate": 1,
-                                "availableBalance":3000
+                                "standingInst": 400,
+                                "balance": 4000,
+                                "minBalance": 100,
+                                "interestRate": 0.25,
+                                "availableBalance":3500
                             }
                         ]
                     }
@@ -169,53 +167,51 @@ module.exports = {
             ],
             creditBankAccountDetails: [
                 {
-                    bankName: 'Capital One',
-                    bankId: 'CCaappiittaall  OOnnee',
+                    bankName: 'JP Morgan Chase',
+                    bankId: 'JJPP  MMoorrggaann  CChhaassee',
                     accounts: [
                         {
                             accountType: 'CC',
                             accountNumber: '456789 0123456789',
-                            accountTitle: 'Low Rate',
-                            creditLimit: 4000,
-                            availableCredit: 2000,
-                            totalBalanceDue: 2000,
-                            minBalanceDue: 500,
-                            apr: 35,
+                            accountTitle: 'Chase Freedom Unlimited Credit Card',
+                            creditLimit: 3000,
+                            availableCredit: 200,
+                            totalBalanceDue: 2800,
+                            minBalanceDue: 30,
+                            apr: 25.49,
                             dueDate: new Date().setDate(new Date().getDate()+12)                         
                         }
                     ]
                 },
                 {
-                    bankName: 'Barclays',
-                    bankId: 'BBaarrccllaayyss',
+                    bankName: 'Bank of Scotland',
+                    bankId: 'BBaannkk  ooff  SSccoottllaanndd',
                     accounts: [
                         {
                             accountType: 'CC',
                             accountNumber: '567890 1234567890',
-                            accountTitle: 'No Fee 0% Balance Transfer',
-                            creditLimit: 4000,
-                            availableCredit: 1500,
-                            totalBalanceDue: 2500,
-                            minBalanceDue: 400,
-                            apr: 30,
+                            accountTitle: 'Classic Credit Card',
+                            creditLimit: 1200,
+                            availableCredit: 800,
+                            totalBalanceDue: 400,
+                            minBalanceDue: 100,
+                            apr: 27.9,
                             dueDate: new Date().setDate(new Date().getDate()+5)                          
                         }
                     ]
                 },
                 {
-                    bankName: 'Citi Bank',
-                    bankId: 'CCiittii  BBaannkk',
+                    bankName: 'Monzo',
+                    bankId: 'MMoonnzzoo',
                     accounts: [
                         {
                             accountType: 'M',
                             accountNumber: '567890 1234567890',
                             accountTitle: 'Home Loan',
-                            minMonthlyPayment: 150,
-                            remainingFullTerm: '1Y 10M',
-                            totalBalanceDue: 2000,
-                            minBalanceDue: 2000,
-                            interestRate: 2.5,
-                            apr: 25,
+                            minMonthlyPayment: 900,
+                            remainingFullTerm: '0Y 5M',
+                            totalBalanceDue: 6000,
+                            interestRate: 2.9,
                             dueDate: new Date().setDate(new Date().getDate()+20)                         
                         }
                     ]
