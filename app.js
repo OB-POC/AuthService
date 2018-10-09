32c0b13dd38d081df543bcabd7304c7192b19f88
vinitkumargoel <vinit928@gmail.com>
2018-10-09 16:32:12 +0530

    DOcker error fixed

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,19 +20,21 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(process.env.NODE_ENV == "PRO" ? '/authService' : "/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
