c48b8ee105d34ca6f90c8abdf68c33d65a3e2c68
HA326076 <hari.n93@wipro.com>
2018-12-21 21:22:49 +0530

    removed config reference

--- routes/index.js
+++ routes/index.js
@@ -41,12 +41,19 @@ router.post('/login', (req, res) => {
 });
 
 router.get('/authenticate', function(req, res){
-  var token = req.headers['x-access-token'];
+  try{
+    var token = req.headers['x-access-token'];
 
-  jwt.verify(token, config.secret , function(err, decodedObj){
+  jwt.verify(token, secret , function(err, decodedObj){
     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
     res.status(200).send(decodedObj);
   })
+  }
+  catch(err)
+  {
+    console.log(err);
+  }
+  
 });
 
 module.exports = router;
